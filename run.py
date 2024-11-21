1deaa1a0e9be702021fcf168ffadd12b1ee90621
Leonardo Barraza <leonardoa-barrazac@unilibre.edu.co>
2024-11-20 20:46:43 -0500

    Se añadieron más documentos

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+'''
+#from flask import Flask, render_template
 from flask_cors import CORS
 
 # Crear la aplicación Flask
@@ -12,4 +13,20 @@ def index():
 
 # Iniciar la aplicación
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True)#
+'''
+from venv import create
+from login.app import create_app, db
+from login.app.auth.models import User
+
+
+flask_scrapy_app =  create_app("prod")
+with flask_scrapy_app.app_context():
+    db.create_all()
+    if not User.query.filter_by(user_name="test").first():
+        User.create_user(
+            user="test",
+            email="test-testing@test.com",
+            password="test**123"
+        )
+flask_scrapy_app.run()
